fb5f10b390f6f23befab3871cf43594ed0f75c33
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-11-26 11:02:32 +0000

    Set up the search results controller and view

---
+++ app/controllers/search_results_controller.rb
@@ -0,0 +1,7 @@
+class SearchResultsController < ApplicationController
+  def display_advisor_sign_in?
+    false
+  end
+
+  helper_method :display_advisor_sign_in?
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  # first route for the show.html.erb view
   get '/', to: 'show#show'
+
+  get '/search_results', to: 'search_results#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
