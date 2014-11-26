39d48b0f8bc2ee6ca51837c74b20aae022375787
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-11-26 14:36:56 +0000

    Add data for the results partial

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -11,13 +11,48 @@ class SearchResultsController < ApplicationController
 
   helper_method :display_search_results_total?
 
+  def show
+    @advisor_data = advisor_data
+  end
+  
+  private
+
   def advisor_data
     items = [
       {
         advisor_name: 'Joe Bloggs',
         advisor_number: 'FCA authorised: #233414234 since October 1998',
         location: 'Location: Essex (12 miles away)',
-        percentage_match: '72% match to your requorements'
+        percentage_match: '65% match to your requirements',
+        percentage: '65'
+      },
+      {
+        advisor_name: 'Ajit Krishnan',
+        advisor_number: 'FCA authorised: #233234234 since September 1995',
+        location: 'Location: South London (32 miles away)',
+        percentage_match: '72% match to your requirements',
+        percentage: '72'
+      },
+      {
+        advisor_name: 'Ben Barnett',
+        advisor_number: 'FCA authorised: #233765835 since June 1990',
+        location: 'Location: Essex (19 miles away)',
+        percentage_match: '88% match to your requirements',
+        percentage: '88'
+      },
+      {
+        advisor_name: 'Amandip Singh',
+        advisor_number: 'FCA authorised: #348992876 since April 1987',
+        location: 'Location: East London (22 miles away)',
+        percentage_match: '52% match to your requirements',
+        percentage: '52'
+      },
+      {
+        advisor_name: 'Peter Guntrip',
+        advisor_number: 'FCA authorised: #392856746 since January 2001',
+        location: 'Location: Essex (8 miles away)',
+        percentage_match: '66% match to your requirements',
+        percentage: '66'
       }
     ]
   end
