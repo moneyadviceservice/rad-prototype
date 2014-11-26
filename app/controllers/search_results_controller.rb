5928eff721e00f3fcb024a5741584082481c8983
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-11-26 13:14:59 +0000

    Add temp fake data to the controller

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -4,4 +4,21 @@ class SearchResultsController < ApplicationController
   end
 
   helper_method :display_advisor_sign_in?
+
+  def display_search_results_total?
+    false
+  end
+
+  helper_method :display_search_results_total?
+
+  def advisor_data
+    items = [
+      {
+        advisor_name: 'Joe Bloggs',
+        advisor_number: 'FCA authorised: #233414234 since October 1998',
+        location: 'Location: Essex (12 miles away)',
+        percentage_match: '72% match to your requorements'
+      }
+    ]
+  end
 end
